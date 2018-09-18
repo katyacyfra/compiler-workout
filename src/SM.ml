72b4ca5ab48c809b44afce9d53b678a98c3c6373
katyacyfra <31743249+katyacyfra@users.noreply.github.com>
2018-09-18 12:33:24 +0300

    hw3

--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,35 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let rec eval cfg prg = match prg with
+        | [] -> cfg
+        | p :: ps -> match p with
+            | BINOP binop -> (match cfg with
+                | (x :: y :: xs, (st, inp, out)) ->  eval (((Language.Expr.eval st (Binop (binop, Const y, Const x)))) :: xs, (st, inp, out)) ps
+                | _ -> failwith "eval BINOP failed!"
+			)
+			| CONST c -> (match cfg with
+                | (xs, stcfg) ->  eval (c :: xs, stcfg) ps
+                | _ -> failwith "eval CONST failed!"
+			)
+			| READ -> (match cfg with
+                | (xs, (st, i :: inp, out)) ->  eval (i :: xs, (st, inp, out)) ps
+                | _ -> failwith "eval READ failed!"
+			)
+			| WRITE -> (match cfg with
+                | (x :: xs, (st, inp, out)) ->  eval (xs, (st, inp, out @ [x])) ps
+                | _ -> failwith "eval WRITE failed!"
+			)
+			| LD var -> (match cfg with
+                | (xs, (st, inp, out)) ->  eval ((st var) :: xs, (st, inp, out)) ps
+                | _ -> failwith "eval LD failed!"
+			)
+			| ST var -> (match cfg with
+                | (x :: xs, (st, inp, out)) ->  eval (xs, ((Language.Expr.update var x st), inp, out)) ps
+                | _ -> failwith "eval LD failed!"
+			)
+			| _ -> failwith "eval unknown instruction!"
+
 
 (* Top-level evaluation
 
@@ -41,4 +69,15 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compileHelper expr = 
+    match expr with
+        | Language.Expr.Var x -> [LD x]
+        | Language.Expr.Const n -> [CONST n]
+        | Language.Expr.Binop (op, l, r) -> compileHelper l @ compileHelper r @ [BINOP op]		
+
+let rec compile sttype = 
+    match sttype with
+	    | Language.Stmt.Read var -> [READ; ST var]
+        | Language.Stmt.Write expr -> compileHelper expr @ [WRITE]
+        | Language.Stmt.Assign (var, value) -> compileHelper value @ [ST var]
+        | Language.Stmt.Seq(firstst, secondst) -> compile firstst @ compile secondst
